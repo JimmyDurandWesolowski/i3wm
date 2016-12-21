f35b1afa5516cc27bf6379258c736a6803c416f9
Samuel W. Flint <swflint@flintfam.org>
2016-12-21 15:17:36 -0600

    Updated thanks to package-lint

--- i3wm.el
+++ i3wm.el
@@ -1,11 +1,11 @@
-;;; i3wm.el --- i3wm integration library for emacs
+;;; i3wm.el --- i3wm integration library
 
 ;; Copyright (C) 2016 Samuel Flint
 
 ;; Author: Samuel W. Flint <swflint@flintfam.org>
 ;; Version: 0.5
-;; Package-requires: ((cl-lib "0.5") json)
-;; Keywords: i3
+;; Package-requires: ((cl-lib "0.5") (json))
+;; Keywords: convenience, exteensions
 ;; URL: https://git.flintfam.org/swflint/emacs-i3wm
 
 ;;; Commentary:
@@ -18,7 +18,7 @@
 (require 'json)
 (require 'cl-lib)
 
-(defun i3-command (command &rest arguments)
+(defun i3wm-command (command &rest arguments)
   "I3-command COMMAND &rest ARGUMENTS.
 
 Execute the givenn COMMAND with the given ARGUMENTS."
@@ -26,21 +26,21 @@ Execute the givenn COMMAND with the given ARGUMENTS."
    (shell-command-to-string
     (format "i3-msg \"%s\"" (apply #'format command arguments)))))
 
-(defun i3-get-workspaces ()
+(defun i3wm-get-workspaces ()
   "I3-get-workspaces.
 
 List all workspaces."
   (json-read-from-string
    (shell-command-to-string "i3-msg -t get_workspaces")))
 
-(defun i3-get-outputs ()
+(defun i3wm-get-outputs ()
   "I3-get-outputs.
 
 List all outputs."
   (json-read-from-string
    (shell-command-to-string "i3-msg -t get_outputs")))
 
-(defun i3-get-version ()
+(defun i3wm-get-version ()
   "I3-get-version.
 
 Retrieve i3 version."
