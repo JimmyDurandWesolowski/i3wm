f6989fbbca270bc0d5c74023ea28d5ef7e840a7d
Samuel W. Flint <swflint@flintfam.org>
2016-12-21 14:42:22 -0600

    Added the skeleton

---
+++ i3wm.el
@@ -0,0 +1,25 @@
+;;; i3wm.el --- i3wm integration library for emacs
+
+;; Copyright (C) 2016 Samuel Flint
+
+;; Author: Samuel W. Flint <swflint@flintfam.org>
+;; Version: 0.5
+;; Package-requires: ((cl-lib "0.5") json)
+;; Keywords: i3
+;; URL: https://git.flintfam.org/swflint/emacs-i3wm
+
+;;; Commentary:
+;;
+
+
+
+;;; Code:
+
+(require 'json)
+(require 'cl-lib)
+
+
+
+(provide 'i3wm)
+
+;;; i3wm.el ends here
