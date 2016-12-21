cd96eb6a023eb8b9608363c705eef0a1d86c03fa
Samuel W. Flint <swflint@flintfam.org>
2016-12-21 15:38:45 -0600

    Updated the docstrings

--- i3wm.el
+++ i3wm.el
@@ -19,7 +19,7 @@
 (require 'cl-lib)
 
 (defun i3wm-command (command &rest arguments)
-  "I3-command COMMAND &rest ARGUMENTS.
+  "I3wm-command COMMAND &rest ARGUMENTS.
 
 Execute the givenn COMMAND with the given ARGUMENTS."
   (json-read-from-string
@@ -27,21 +27,21 @@ Execute the givenn COMMAND with the given ARGUMENTS."
     (format "i3-msg \"%s\"" (apply #'format command arguments)))))
 
 (defun i3wm-get-workspaces ()
-  "I3-get-workspaces.
+  "I3wm-get-workspaces.
 
 List all workspaces."
   (json-read-from-string
    (shell-command-to-string "i3-msg -t get_workspaces")))
 
 (defun i3wm-get-outputs ()
-  "I3-get-outputs.
+  "I3wm-get-outputs.
 
 List all outputs."
   (json-read-from-string
    (shell-command-to-string "i3-msg -t get_outputs")))
 
 (defun i3wm-get-version ()
-  "I3-get-version.
+  "I3wm-get-version.
 
 Retrieve i3 version."
   (json-read-from-string
